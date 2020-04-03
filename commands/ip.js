b6b1b8cebdbcb422eecf69d971ea2b14797123e8
RiemerHF <riemer.fongers@gmail.com>
2020-04-03 18:22:16 +0200

    Ip command added.

--- commands/ip.js
+++ commands/ip.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) =>{
 
-  return message.reply('hi.');
+  return message.channel.send('play.farmcraft.com');
 
 }
 
