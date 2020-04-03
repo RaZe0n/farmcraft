21a5d7edb6b706e6e66639a1f05304fbb8126a60
RaZe0n <finn.bruinzeel@gmail.com>
2020-04-03 23:19:59 +0200

    asdasd

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ bot.on("guildMemberAdd", member =>{
     var welcomeEmbed = new discord.MessageEmbed()
       .setTitle(`Welkom op de server ${member.user.username}!` )
       .setColor("#f16411")
-      .setDescription("Veel plezier op de server!");
-      .setThumbnail("https://imgur.com/a/Mk3DzNN")
-      .setImage("https://imgur.com/a/Mk3DzNN")
+      .setDescription("Veel plezier op de server!")
+      .setThumbnail("https://imgur.com/a/Mk3DzNN.png")
+      .setImage("https://imgur.com/a/Mk3DzNN.png");
 
     const welcomeChannel = member.guild.channels.cache.find(c => c.name === "welkom");
     if(!welcomeChannel) return;
