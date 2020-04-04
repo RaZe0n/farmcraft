7ada44ab45842813625edb5ec71ad022a4e5cd88
RaZe0n <finn.bruinzeel@gmail.com>
2020-04-04 17:38:57 +0200

    asdasdga

---
+++ commands/closeembed.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) =>{
+
+
+  var closeEmbed = new discord.MessageEmbed()
+    //.setTitle(`Welkom op de server` )
+    .setTitle(`${message.guild.name} Tickets`, message.guild.iconURL)
+    .setColor("#f16411")
+    .setDescription("Deze ticket zal over 10 seconden sluiten!")
+    .setTimestamp()
+    .setFooter("Gesloten:");
+
+  return message.channel.send(closeEmbed);
+
+}
+
+module.exports.help = {
+  name: "closeembed"
+}
