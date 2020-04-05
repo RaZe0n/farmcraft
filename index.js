dff5b7a96c5127220ca55784ab39e837cf16981f
RiemerHF <riemer.fongers@gmail.com>
2020-04-05 20:38:59 +0200

    Ticket sys

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ bot.on("message", message => {
   if(message.content.toLowerCase() === '!rename') {
     if(!message.member.hasPermission("MANAGE_CHANNELS")) return;
 
-    const renameArgs = message.content.slice(prefix.length).split(' ');
+    var renameArgs = message.content.slice(prefix.length).split(' ');
 
     const categoryId = '680478197795520614'
 
