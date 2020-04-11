818cc1424f263521a68e535d814d52a155fe5bb1
RiemerHF <riemer.fongers@gmail.com>
2020-04-11 14:50:51 +0200

    fu

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
         }
       ]
     }).then(channel => {
-    let category = reaction.message.guild.channels.cache.find(c => c.name == "「🎫」ticket" && c.type == "category");
+    let category = reaction.message.guild.channels.cache.find(c => c.name == "support" && c.type == "category");
 
     //let supportChannel = reaction.message.guild.channels.cache.find(c => c.name == `${user.username.toLowerCase()}-ticket`);
 
